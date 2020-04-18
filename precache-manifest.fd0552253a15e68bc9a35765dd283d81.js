3b0fa2fd31e662942aabdd65b4f3a6df7f8c664f
AungPhoneKhant <akha0030@student.monash.edu>
2020-04-18 13:53:15 +1000

    Updates

---
+++ precache-manifest.fd0552253a15e68bc9a35765dd283d81.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b989590cba8524e179380bc1dbc487e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8093780450afce15b59f",
+    "url": "/static/css/2.5c1d7e3b.chunk.css"
+  },
+  {
+    "revision": "c0624cef8074bc802d81",
+    "url": "/static/css/main.6b2f918b.chunk.css"
+  },
+  {
+    "revision": "8093780450afce15b59f",
+    "url": "/static/js/2.e0ec638a.chunk.js"
+  },
+  {
+    "revision": "b238709882e4cd60e110f01a26ae78cd",
+    "url": "/static/js/2.e0ec638a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0624cef8074bc802d81",
+    "url": "/static/js/main.ed86824f.chunk.js"
+  },
+  {
+    "revision": "058c101e6d1679597414",
+    "url": "/static/js/runtime-main.d2fc6d6f.js"
+  },
+  {
+    "revision": "79dee7c7ea3b21b7970003c820f500c1",
+    "url": "/static/media/ObjectSans-Heavy.79dee7c7.otf"
+  },
+  {
+    "revision": "5b82442dbf02699c000fb5d8587c3ca3",
+    "url": "/static/media/ObjectSans-Regular.5b82442d.otf"
+  }
+]);
